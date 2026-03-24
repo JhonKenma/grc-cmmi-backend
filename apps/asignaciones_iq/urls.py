a9c7bd6655ecc8fdac2637c85cfd04bf708fdcf6
Jhons Kenma <jhonskenma@gmail.com>
2026-03-24 00:12:53 -0500

    feat: update urls for asignaciones_iq

--- apps/asignaciones_iq/urls.py
+++ apps/asignaciones_iq/urls.py
@@ -5,12 +5,15 @@ URLs para Asignaciones de Evaluaciones Inteligentes
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from apps.asignaciones_iq.views_auditor import AuditorIQViewSet
 from .views import AsignacionEvaluacionIQViewSet, RespuestaEvaluacionIQViewSet, EvidenciaIQViewSet
 
 router = DefaultRouter()
 router.register(r'asignaciones-iq', AsignacionEvaluacionIQViewSet, basename='asignacion-evaluacion-iq')
 router.register(r'respuestas-iq', RespuestaEvaluacionIQViewSet, basename='respuesta-evaluacion-iq')
 router.register(r'evidencias-iq', EvidenciaIQViewSet, basename='evidencia-iq')
+router.register(r'auditor-iq', AuditorIQViewSet, basename='auditor-iq')
 
 urlpatterns = [
     path('', include(router.urls)),
