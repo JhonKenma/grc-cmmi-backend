206c894156389ccf67d849d456d1565e8588a5a1
Jhons Kenma <jhonskenma@gmail.com>
2026-03-26 11:44:32 -0500

    feat(usuarios): actualización de rutas

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,7 +1,7 @@
 # apps/usuarios/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView  # ← agregar TokenBlacklistView
 from .views import UsuarioViewSet, CustomTokenObtainPairView
 
 router = DefaultRouter()
@@ -10,5 +10,6 @@ router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),  # ← agregar
     path('', include(router.urls)),
 ]
