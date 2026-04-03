978463fb81469f81a9fc481597d4e1edd10c0253
Jhons Kenma <jhonskenma@gmail.com>
2026-04-02 21:27:13 -0500

    chore(config): actualizar configuración del proyecto

--- config/settings.py
+++ config/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.usuarios.middleware.PlanExpirationMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
